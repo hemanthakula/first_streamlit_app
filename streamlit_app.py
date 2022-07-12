c09c518dfc37560c2b6351093fbc777cfc61d196
hemanthakula <akula.hemanth111@gmail.com>
2022-07-12 21:57:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ streamlit.title (" My parents are in Home")
 
 streamlit.header("Today Menu Break Fast")
 streamlit.text("1. 3 Mangoes")
-streamlit.tect("2. Bananas & Cake")
+streamlit.text("2. Bananas & Cake")

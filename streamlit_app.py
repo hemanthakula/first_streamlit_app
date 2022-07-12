0fc48af4dcc86f5bda60abed10924813a5835fa0
hemanthakula <akula.hemanth111@gmail.com>
2022-07-12 22:00:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title (" My parents are in Home")
-
 streamlit.header("Today Menu Break Fast")
 streamlit.text("1. 3 Mangoes")
 streamlit.text("2. Bananas & Cake")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

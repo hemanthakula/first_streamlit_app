d7d95bd6149865e5d93b73fc766254d4ca8c1ba2
hemanthakula <akula.hemanth111@gmail.com>
2022-07-12 22:05:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text(' 🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas 
